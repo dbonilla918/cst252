b6c4e19cbdd5028f46d8021d77548413febbafd7
dbonilla918 <78111889+dbonilla918@users.noreply.github.com>
2022-09-13 20:45:09 -0700

    started and finished all required material for Lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,17 +1,31 @@
 function flowerFunction() {
 	//initialize();
-	
-	//YOUR CODE GOES RIGHT HERE 
+
+	//YOUR CODE GOES RIGHT HERE
 
 	document.querySelector("#flower5 .center").textContent = document.querySelector("#flower1 .center").textContent + document.querySelector("#flower3 .center").textContent;
-	document.querySelector("#flower5 .center").style.backgroundColor = "purple";
-	document.querySelector("#flower5 .center").style.color = "white";
+	document.querySelector("#flower5 .center").style.backgroundColor = 'white'
+	document.querySelector("#flower5 .center").style.color = 'red';
 
-	document.querySelector(`#flower1`).style.marginTop = "100px";
-	document.querySelector(`#flower2`).style.marginTop = "100px";
-	document.querySelector(`#flower3`).style.marginTop = "100px";
-	document.querySelector(`#flower4`).style.marginTop = "100px";
-	document.querySelector(`#flower5`).style.marginTop = "100px";
+	//these lines of ccode change the vertical positio of all the flowers
+	document.querySelector(`#flower1`).style.marginTop = "0px";
+	document.querySelector(`#flower2`).style.marginTop = "0px";
+	document.querySelector(`#flower3`).style.marginTop = "0px";
+	document.querySelector(`#flower4`).style.marginTop = "0px";
+	document.querySelector(`#flower5`).style.marginTop = "0px";
+
+	//this code changes the first flower to say hi
+	document.querySelector('#flower1 .center').textContent = "hi";
+	//this code changes the third flower to say 2
+	document.querySelector('#flower3 .center').textContent = "2";
+	//this code changes the first flower to say hi2
+	document.querySelector('#flower5 .center').textContent = "hi2";
+
+	//This line of code changes the left petal of flower 5 from orange to lightblue
+	document.querySelector("#flower5 .petal").style.backgroundColor = 'lightblue'
+	//This line of code changes the right petal of flower 5 from orange to pink
+	document.querySelector("#flower5 .rightPetal").style.backgroundColor = 'pink'
+
 
 	// DO NOT PUT ANY CODE AFTER THIS POINT
 }
@@ -21,7 +35,7 @@ function initialize() {
 	//DO NOT CHANGE ANY OF THIS CODE
 	document.querySelector("#flower1 .center").textContent = randomNumber();
 	document.querySelector("#flower3 .center").textContent = randomNumber();
-		
+
 
 
 }

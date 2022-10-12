16a9874934e9b7c43fa21a9c4a84b651b6ede989
dbonilla918 <78111889+dbonilla918@users.noreply.github.com>
2022-10-11 21:19:40 -0700

    finalized lab 10

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -12,39 +12,24 @@
 //
 // 3. what happens when the event happens
 //     -the color of the courseBox box changes
-//         let highlightBoxFunc =
-//
-//
 //     --the color of the courseBox goes back to normal
+//     ---the info panel shows up
+//     update course name
+//     update course description
+//     change the picture
 //
-
-    //---the info panel shows up
-        //update course name
-        //update course description
-        //change the picture
-
-// document.addEventListener("DOMContentLoaded", () => {
-//
-// // all your code goes here!
-//
-//
-//     //----closes the info panel
-// });
-// let panel = document.querySelector('#infopanel')
+//     ----the info panel hides but keeps the data from
+//         the last box the user cliked
 
 
+document.addEventListener("DOMContentLoaded", () => {
 
-// let closePanel = (x) => {
-//     console.log(closePanel);
-//     infopanel.style.display = "none";
-// };
+    // this function makes the info panel hide on load
+    let infoPanelBox = document.querySelector('#infopanel');
+    infoPanelBox.style.display = "none";
 
-
-// document.addEventListener("DOMContentLoaded", () => {
-    //'#infopanel'.style.display = "none";
-
+    //funtion shows info panel, updates course name, info, and image based off click event
     let showPanel = (x) => {
-
 
         console.log(x);
 
@@ -67,6 +52,29 @@
         courseImage = document.querySelector('#courseimage').src="img/cst252.jpg";
     }
 
-// });
+    //allows user to click on the box and trigger the info panel to show up on screen
+    let cst252Box = document.querySelector('.itembox');
+    cst252Box.addEventListener('click', showPanel);
 
-//////////////////
+    //function created to close the info panel
+    let closeThePanel = () => {
+        let panel = document.querySelector('#infopanel');
+        panel.style.display = "none";
+    }
+
+    //makes the exit button on the top right hide the whole info panel
+    let exitButtonClose = document.querySelector('#exitbutton')
+    exitButtonClose.addEventListener('click', closeThePanel);
+
+    //This is for the mouseover and mouseout events
+    document.querySelector('.itembox').onmouseover = function() {mouseOver()};
+    document.querySelector('.itembox').onmouseout = function() {mouseOut()};
+
+    function mouseOver() {
+      document.querySelector('.itembox').style.borderColor = "lightblue";
+    }
+
+    function mouseOut() {
+      document.querySelector('.itembox').style.borderColor = "grey";
+    }
+});
